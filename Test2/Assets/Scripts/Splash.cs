a9686879882859eeb8941e908147dff156939216
Elizabeth <evilla40@uic.edu>
2019-04-02 20:59:54 -0500

    Beini's package

---
+++ Test2/Assets/Scripts/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+    public ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ps.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
