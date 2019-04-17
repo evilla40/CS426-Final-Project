fcc3a570a9aaf03bb90d9638190566691a4e79c9
cmondragon15117 <cmondragon15117@gmail.com>
2019-04-17 15:21:53 -0500

    del

--- Test2/Assets/Scripts/Restart.cs
+++ Test2/Assets/Scripts/Restart.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ced148727546380d73bbbc177be567aa9520975d78cf8c01356af583148ee4d3
-size 439
+oid sha256:bb24351a9c3e1f159b7b8409e2f8da94285eccfd49a9d0874a56eff5074704aa
+size 440
